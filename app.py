63a3ba5f9ed05a65c347e65f2eff065e338ebce6
eomseung-yeol <co6747@naver.com>
2022-09-27 19:32:12 +0900

    update : pandas index is out of bounds 해결.

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ from sklearn.metrics.pairwise import linear_kernel
 app = Flask(__name__)
 
 def db_connector():
-    db=pymysql.connect(host='XXXXX',
-                      user='XXXXX',
+    db=pymysql.connect(host='XXXX',
+                      user='XXXX',
                       password='XXXX',
-                      db='XX',
-                      charset='XX')
+                      db='XXXX',
+                      charset='XXX')
     cursor = db.cursor()
     sql='''SELECT * FROM korean_food;'''
     cursor.execute(sql)
@@ -37,6 +37,7 @@ def ML():
 
     # 사용하고자 하는 csv 데이터 셋을 pandas의 read_csv 메서드를 통해서 불러온다.
     wte_food_data = pd.read_csv('/Users/eomseung-yeol/PycharmProjects/Flask_Prac/wte_food_data.csv')
+    # pd.set_option('display.max_columns', 0)
     df = pd.DataFrame(wte_food_data)
     dfInit = df.query('feature.str.contains("")')  # 초기에 비선호 데이터를 추출하기 위한 DataFrame 초기화.
     print("--------------")
@@ -46,16 +47,16 @@ def ML():
 
     # 넘겨받은 값을 기반으로 데이터를 추출한다.
     for i in value.split(","):
-        dfInit = dfInit.query('feature.str.contains(@i)==False')
+        newDfInit = dfInit.query('feature.str.contains(@i)==False')
         print("-------")
         print(i)
 
     print("csv 필터링 데이터.")
-    print(dfInit)  # csv 파일에서 필터링 한 데이터.
+    print(newDfInit)  # csv 파일에서 필터링 한 데이터.
 
-    tfidf = TfidfVectorizer().fit(dfInit)  # tfidf 알고리즘을 동작시키기 위한 vectorizer 객체.
+    tfidf = TfidfVectorizer().fit(newDfInit)  # tfidf 알고리즘을 동작시키기 위한 vectorizer 객체.
     tfidf_matrix = tfidf.fit_transform(
-        dfInit.feature)  # 비교해야되는 대상이 각 음식이 가지고 있는 feature 이기 때문에 데이터에서 feature 칼럼만 추출.
+        newDfInit.feature)  # 비교해야되는 대상이 각 음식이 가지고 있는 feature 이기 때문에 데이터에서 feature 칼럼만 추출.
     # print("tfidf_matrix")
     # print(tfidf_matrix.shape) #feature에 대해서 tf-idf를 진행한 결과값 => (80,92) 80->feature 갯수 ,92-> 서로 다른 유니크한 속성값의 갯수
 
@@ -64,7 +65,7 @@ def ML():
     # print(res_cosine)
 
     # pandas 시리즈 배열을 통해서, 인덱스가 음식명이고, 인덱스번호가 값인 판다스 시리즈 배열 선언.
-    idx = pd.Series(dfInit.index, index=dfInit.name).drop_duplicates()
+    idx = pd.Series(newDfInit.index, index=newDfInit.name).drop_duplicates()
 
     def food_chk(name, res_cosine=res_cosine):
         # 유저가 선택한 음식 카테고리를 기반으로 인덱스 번호를 추출한다. -> flask 에서 get 요청으로 받은 값을 기반으로, 진행한다.
@@ -76,13 +77,13 @@ def ML():
         # 코싸인 유사도에 따라 음식데이터를 정렬한다.
         sims_cosine = sorted(sims_cosine, key=lambda x: x[1], reverse=True)
 
-        # 가장 유사한 데이터 5개를 받아온다.
+        # 가장 유사한 데이터 2개를 받아온다.
         sims_cosine = sims_cosine[1:3]
 
         # 가장 유사한 데이터 5개의 인덱스를 받아온다.
         food_idx = [i[0] for i in sims_cosine]
 
-        result_df = dfInit.iloc[food_idx].copy()
+        result_df = newDfInit.iloc[food_idx].copy()
         result_df['score'] = [i[1] for i in sims_cosine]
 
         # 결과값에서 특징 속성 제거.
