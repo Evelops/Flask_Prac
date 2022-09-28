84ec1aa71b18d4083dc554041d64585b0ad8c474
eomseung-yeol <co6747@naver.com>
2022-09-28 16:53:54 +0900

    update : pandas score 값 기준 정렬.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pymysql
 import json
 import pandas as pd
@@ -13,7 +14,7 @@ def db_connector():
                       user='XXXX',
                       password='XXXX',
                       db='XXXX',
-                      charset='XXX')
+                      charset='XXXX')
     cursor = db.cursor()
     sql='''SELECT * FROM korean_food;'''
     cursor.execute(sql)
@@ -84,7 +85,7 @@ def ML():
         food_idx = [i[0] for i in sims_cosine]
 
         result_df = newDfInit.iloc[food_idx].copy()
-        result_df['score'] = [i[1] for i in sims_cosine]
+        result_df['score'] =np.round([i[1] for i in sims_cosine], 2) * 100
 
         # 결과값에서 특징 속성 제거.
         del result_df['feature']
@@ -100,16 +101,16 @@ def ML():
 
     # AOS에서 받아오는 최대 길이가 5 뿐이 안되기 때문에, 임시로 받아오는 데이터의 길이에 따라서, tf-idf 결과 값을 리턴한다.
 
-    if len(getFoodList) == 1 :
+    if len(getFoodList) == 1:
         # print("len -> "+len(getFoodList))
         res = pd.concat([food_chk(getFoodList[0])], ignore_index=True)
-    elif len(getFoodList) == 2 :
+    elif len(getFoodList) == 2:
         # print("len -> "+len(getFoodList))
         res = pd.concat([food_chk(getFoodList[0]),food_chk(getFoodList[1])], ignore_index=True)
-    elif len(getFoodList) == 3 :
+    elif len(getFoodList) == 3:
         # print("len -> "+len(getFoodList))
         res = pd.concat([food_chk(getFoodList[0]),food_chk(getFoodList[1]),food_chk(getFoodList[2])], ignore_index=True)
-    elif len(getFoodList) == 4 :
+    elif len(getFoodList) == 4:
         # print("len -> "+len(getFoodList))
         res = pd.concat([food_chk(getFoodList[0]),food_chk(getFoodList[1]),food_chk(getFoodList[2]),food_chk(getFoodList[3])], ignore_index=True)
     else:
@@ -117,71 +118,72 @@ def ML():
         res = pd.concat([food_chk(getFoodList[0]),food_chk(getFoodList[1]),food_chk(getFoodList[2]),food_chk(getFoodList[3]),food_chk(getFoodList[4])], ignore_index=True)
 
     print(res)
+    # res.sort_values('score',ascending=False)
     resVal = json.dumps(res.to_dict('records'), ensure_ascii=False,indent=4)
 
-    print(json.dumps(res.to_dict('records'), ensure_ascii=False, indent=4))
+    print(json.dumps(res.sort_values('score',ascending=False).to_dict('records'), ensure_ascii=False, indent=4))
 
     return resVal
 
 
-# 최종적으로 결과값을 리턴하는 값.
-def resQuery(value):
-    # 사용하고자 하는 csv 데이터 셋을 pndas의 read_csv 메서드를 통해서 불러온다.
-    wte_food_data = pd.read_csv('/Users/eomseung-yeol/PycharmProjects/Flask_Prac/wte_food_data.csv')
-    df = pd.DataFrame(wte_food_data)
-    dfInit = df.query('feature.str.contains("")') # 초기에 비선호 데이터를 추출하기 위한 DataFrame 초기화.
-    print("--------------")
-    print(dfInit)
-    print("--------------")
-    print(df)
+# # 최종적으로 결과값을 리턴하는 값.
+# def resQuery(value):
+#     # 사용하고자 하는 csv 데이터 셋을 pndas의 read_csv 메서드를 통해서 불러온다.
+#     wte_food_data = pd.read_csv('/Users/eomseung-yeol/PycharmProjects/Flask_Prac/wte_food_data.csv')
+#     df = pd.DataFrame(wte_food_data)
+#     dfInit = df.query('feature.str.contains("")') # 초기에 비선호 데이터를 추출하기 위한 DataFrame 초기화.
+#     print("--------------")
+#     print(dfInit)
+#     print("--------------")
+#     print(df)
+#
+#     # 넘겨받은 값을 기반으로 데이터를 추출한다.
+#     for i in value.split(","):
+#         dfInit = dfInit.query('feature.str.contains(@i)==False')
+#
+#
+#     print(dfInit) # csv 파일에서 필터링 한 데이터.
+#
+#     tfidf = TfidfVectorizer().fit(dfInit)  # tfidf 알고리즘을 동작시키기 위한 vectorizer 객체.
+#     tfidf_matrix = tfidf.fit_transform(
+#         dfInit.feature)  # 비교해야되는 대상이 각 음식이 가지고 있는 feature 이기 때문에 데이터에서 feature 칼럼만 추출.
+#     # print("tfidf_matrix")
+#     # print(tfidf_matrix.shape) #feature에 대해서 tf-idf를 진행한 결과값 => (80,92) 80->feature 갯수 ,92-> 서로 다른 유니크한 속성값의 갯수
+#
+#     # tf-idf 로직을 수행한 데이터셋에 대해서 cosine 유사도를 구한 결과값을 리턴.
+#     res_cosine = linear_kernel(tfidf_matrix, tfidf_matrix)
+#     # print(res_cosine)
+#
+#     # pandas 시리즈 배열을 통해서, 인덱스가 음식명이고, 인덱스번호가 값인 판다스 시리즈 배열 선언.
+#     idx = pd.Series(dfInit.index, index=dfInit.name).drop_duplicates()
 
-    # 넘겨받은 값을 기반으로 데이터를 추출한다.
-    for i in value.split(","):
-        dfInit = dfInit.query('feature.str.contains(@i)==False')
 
 
-    print(dfInit) # csv 파일에서 필터링 한 데이터.
-
-    tfidf = TfidfVectorizer().fit(dfInit)  # tfidf 알고리즘을 동작시키기 위한 vectorizer 객체.
-    tfidf_matrix = tfidf.fit_transform(
-        dfInit.feature)  # 비교해야되는 대상이 각 음식이 가지고 있는 feature 이기 때문에 데이터에서 feature 칼럼만 추출.
-    # print("tfidf_matrix")
-    # print(tfidf_matrix.shape) #feature에 대해서 tf-idf를 진행한 결과값 => (80,92) 80->feature 갯수 ,92-> 서로 다른 유니크한 속성값의 갯수
-
-    # tf-idf 로직을 수행한 데이터셋에 대해서 cosine 유사도를 구한 결과값을 리턴.
-    res_cosine = linear_kernel(tfidf_matrix, tfidf_matrix)
-    # print(res_cosine)
-
-    # pandas 시리즈 배열을 통해서, 인덱스가 음식명이고, 인덱스번호가 값인 판다스 시리즈 배열 선언.
-    idx = pd.Series(dfInit.index, index=dfInit.name).drop_duplicates()
-
-
-
-    def food_chk(name, res_cosine=res_cosine):
-        # 유저가 선택한 음식 카테고리를 기반으로 인덱스 번호를 추출한다. -> flask 에서 get 요청으로 받은 값을 기반으로, 진행한다.
-        getId = idx[name]
-
-        # 모든 음식 데이터에 대해서 유사도를 구한다.
-        sims_cosine = list(enumerate(res_cosine[getId]))
-
-        # 코싸인 유사도에 따라 음식데이터를 정렬한다.
-        sims_cosine = sorted(sims_cosine, key=lambda x: x[1], reverse=True)
-
-        # 가장 유사한 데이터 5개를 받아온다.
-        sims_cosine = sims_cosine[1:6]
-
-        # 가장 유사한 데이터 5개의 인덱스를 받아온다.
-        food_idx = [i[0] for i in sims_cosine]
-
-        result_df = dfInit.iloc[food_idx].copy()
-        result_df['score'] = [i[1] for i in sims_cosine]
-
-        # 결과값에서 특징 속성 제거.
-        del result_df['feature']
-        return result_df
-    # result set에 데이터를 "계란김밥" 이런식으로 더미로 넣는게 아니라, AOS 클라이언트에서 받은 데이터를 comma로 분할한 데이터를 넣어서 json 데이터로 추출한다.
-    res = pd.concat([food_chk("계란김밥")],ignore_index=True)
-    return res.to_json(force_ascii=False, orient='records', indent=4)
+    # def food_chk(name, res_cosine=res_cosine):
+    #     # 유저가 선택한 음식 카테고리를 기반으로 인덱스 번호를 추출한다. -> flask 에서 get 요청으로 받은 값을 기반으로, 진행한다.
+    #     getId = idx[name]
+    #
+    #     # 모든 음식 데이터에 대해서 유사도를 구한다.
+    #     sims_cosine = list(enumerate(res_cosine[getId]))
+    #
+    #     # 코싸인 유사도에 따라 음식데이터를 정렬한다.
+    #     sims_cosine = sorted(sims_cosine, key=lambda x: x[1], reverse=True)
+    #
+    #     # 가장 유사한 데이터 5개를 받아온다.
+    #     sims_cosine = sims_cosine[1:6]
+    #
+    #     # 가장 유사한 데이터 5개의 인덱스를 받아온다.
+    #     food_idx = [i[0] for i in sims_cosine]
+    #
+    #     result_df = dfInit.iloc[food_idx].copy()
+    #     result_df['score'] = [i[1] for i in sims_cosine]
+    #
+    #     # 결과값에서 특징 속성 제거.
+    #     del result_df['feature']
+    #     return result_df
+    # # result set에 데이터를 "계란김밥" 이런식으로 더미로 넣는게 아니라, AOS 클라이언트에서 받은 데이터를 comma로 분할한 데이터를 넣어서 json 데이터로 추출한다.
+    # res = pd.concat([food_chk("계란김밥")],ignore_index=True)
+    # return res.to_json(force_ascii=False, orient='records', indent=4)
 
 
 if __name__ == "__main__":
